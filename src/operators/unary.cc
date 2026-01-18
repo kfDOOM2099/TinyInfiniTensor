266f687ddfc45b23990d8b43268d2ac619880565
doom2099 <1274754986@qq.com>
2026-01-18 10:46:29 +0800

    完成作业四：通过 test_cast

--- src/operators/unary.cc
+++ src/operators/unary.cc
@@ -64,22 +64,20 @@ namespace infini
     }
 
     vector<DataType> CastObj::inferDataType(const TensorVec &inputs) const
-    {
+    {   
+        return vector(numOutputs(), getOutputDataType());
         // =================================== 作业 ===================================
-        // TODO：返回经过 cast 操作后, 输出 tensor 的数目和数据类型
-        // REF_FILE: src/core/operator.cc
-        // REF: https://onnx.ai/onnx/operators/onnx__Cast.html#cast-21
+
         // =================================== 作业 ===================================
-        return {};
     }
 
     optional<vector<Shape>> CastObj::inferShape(const TensorVec &inputs)
     {
+        auto shape=inputs[0]->getDims();
         // =================================== 作业 ===================================
-        // TODO：返回经过 cast 操作后的 shape
-        // REF: https://onnx.ai/onnx/operators/onnx__Cast.html#cast-21
+
         // =================================== 作业 ===================================
-        return std::nullopt;
+        return {{shape}};
     }
 
     std::string CastObj::toString() const
